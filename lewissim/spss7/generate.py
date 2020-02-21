565b431a316a59dec298cfc695a85791bf9f8b99
michele-brambilla <michele.brambilla@psi.ch>
2020-02-21 11:26:13 +0100

    Add tool for sps setter/getter generation

---
+++ lewissim/spss7/generate.py
@@ -0,0 +1,25 @@
+
+##########################
+
+# template = """
+# @property
+# def selected_%d(self):
+#     return self._selected_impl(%d)
+# """
+
+#for i in range(1,37):
+#    print(template%(i,i))
+###########################
+
+template = """
+@property
+def select_%d(self):
+    return r_select_impl(%d, self._mask)
+
+@select_%d.setter
+def select_%d(self, value):
+    self._values, self._mask = select_impl(%d, self._values, self._mask)
+"""
+
+for i in range(1,37):
+    print(template%(i,i,i,i,i))
