4b00bb44c728e8cec263c4b0b53a34cd7db41e9d
michele-brambilla <michele.brambilla@psi.ch>
2020-03-02 16:00:26 +0100

    fix issue with mask

--- lewissim/spss7/__init__.py
+++ lewissim/spss7/__init__.py
@@ -1,7 +1,6 @@
 from collections import OrderedDict
 
 from lewis.adapters.epics import PV, EpicsInterface
-from lewis.core import approaches
 from lewis.core.statemachine import State
 from lewis.devices import Device
 
@@ -21,25 +20,34 @@ def select_%d(self):
     return self._select_impl(%d)
 """
 
+
 # for i in range(19,37):
 #     print(template%(i,i))
 
 
 def r_select_impl(pitch, mask):
-    return mask >> int(pitch-1) & 0b1
+    return mask >> int(pitch - 1) & 0b1
 
 
 def select_impl(pitch, value, mask):
-    mask |= (0b1 << pitch-1)
-    return value^mask, mask
+    # print('pitch%d: %d / %d / %d'%(pitch, value, mask, (0b1 << pitch-1)))
+    mask |= (0b1 << pitch - 1)
+    value ^= mask
+    return value, mask
+
+
+def zero_mask(pitch, mask):
+    return mask ^ (0b1 << pitch - 1)
 
 
 def selected_impl(pitch, values):
-    return values >> int(pitch-1) & 0b1
+    # print('pitch%d: %d / %d'%(pitch, values, (values >> int(pitch-1)) & 0b1))
+    return (values >> int(pitch - 1)) & 0b1
 
 
 class SimulatedSps(Device):
-    _fine_adjustment = 0
+    _fine_adjustment1 = 0
+    _fine_adjustment2 = 0
     _fa_select = 0
     _values = 0b0
     _mask = 0b0
@@ -49,9 +57,9 @@ class SimulatedSps(Device):
         for i in range(1, 37):
             for param in ['_select', '_selected', 'selectable',
                           'park_position', 'alarm']:
-                setattr(self, '%s_%d'%(param, i), 0)
-        for i in range(33, 37):
-            setattr(self, '%s_%d'%('selectable', i), 1)
+                setattr(self, '%s_%d' % (param, i), 0)
+        for i in range(1, 37):
+            setattr(self, '%s_%d' % ('selectable', i), 1)
         super(SimulatedSps, self).__init__()
 
     ###################
@@ -208,8 +216,11 @@ class SimulatedSps(Device):
 
     @select_1.setter
     def select_1(self, value):
+        pitch = 1
         if value:
-            self._values, self._mask = select_impl(1, self._values, self._mask)
+            self._values, self._mask = select_impl(pitch, self._values, self._mask)
+        else:
+            self._mask = zero_mask(pitch, self._mask)
 
     @property
     def select_2(self):
@@ -217,8 +228,11 @@ class SimulatedSps(Device):
 
     @select_2.setter
     def select_2(self, value):
+        pitch = 2
         if value:
-            self._values, self._mask = select_impl(2, self._values, self._mask)
+            self._values, self._mask = select_impl(pitch, self._values, self._mask)
+        else:
+            self._mask = zero_mask(pitch, self._mask)
 
     @property
     def select_3(self):
@@ -226,8 +240,11 @@ class SimulatedSps(Device):
 
     @select_3.setter
     def select_3(self, value):
+        pitch = 3
         if value:
-            self._values, self._mask = select_impl(3, self._values, self._mask)
+            self._values, self._mask = select_impl(pitch, self._values, self._mask)
+        else:
+            self._mask = zero_mask(pitch, self._mask)
 
     @property
     def select_4(self):
@@ -235,8 +252,12 @@ class SimulatedSps(Device):
 
     @select_4.setter
     def select_4(self, value):
+        pitch = 4
         if value:
-            self._values, self._mask = select_impl(4, self._values, self._mask)
+            self._values, self._mask = select_impl(pitch, self._values,
+                                                   self._mask)
+        else:
+            self._mask = zero_mask(pitch, self._mask)
 
     @property
     def select_5(self):
@@ -244,8 +265,11 @@ class SimulatedSps(Device):
 
     @select_5.setter
     def select_5(self, value):
+        pitch = 5
         if value:
-            self._values, self._mask = select_impl(5, self._values, self._mask)
+            self._values, self._mask = select_impl(pitch, self._values, self._mask)
+        else:
+            self._mask = zero_mask(pitch, self._mask)
 
     @property
     def select_6(self):
@@ -542,6 +566,8 @@ class SimulatedSps(Device):
         if value:
             self._values, self._mask = select_impl(35, self._values,
                                                    self._mask)
+        else:
+            _, self._mask = select_impl(35, self._values, self._mask)
 
     @property
     def select_36(self):
@@ -552,26 +578,56 @@ class SimulatedSps(Device):
         if value:
             self._values, self._mask = select_impl(36, self._values,
                                                    self._mask)
+        else:
+            _, self._mask = select_impl(36, self._values, self._mask)
 
     ##################
 
+    def _select_impl(self, pitch, value):
+        if value:
+            self._values, self._mask = select_impl(pitch, self._values, self._mask)
+        else:
+            self._mask = zero_mask(pitch, self._mask)
+
     @property
-    def fa_read(self):
-        return self._fine_adjustment
+    def fa_read1(self):
+        return self._fine_adjustment1
+
+    @property
+    def fa_read2(self):
+        return self._fine_adjustment2
 
     @property
     def fa_select(self):
-        return self._fine_adjustment
+        self._find_pitch()
+        fine_adjustment = self._fine_adjustment1 if self._get_mcu_id() == 0 \
+            else self._fine_adjustment2
+        return fine_adjustment
 
     @fa_select.setter
     def fa_select(self, value):
+        fine_adjustment = self._fine_adjustment1
         self._fa_select = 1 if value else 0
-        self._fine_adjustment ^= self._fa_select
+        fine_adjustment ^= self._fa_select
+
+    def _find_pitch(self):
+        for p in range(36):
+            print('pitch%d: %r' % (p + 1, self._values >> p & 0b1))
+        print('')
+
+    def _get_mcu_id(self):
+        mask = int('1' * 18, 2)
+        low = self._values & mask
+        high = self._values & (mask << 18)
+        if low:
+            return 0
+        else:
+            return 1
 
     def _get_state_handlers(self):
         return {
             'idle': State(),
-        }
+            }
 
     def _get_initial_state(self):
         return 'idle'
@@ -581,45 +637,47 @@ class SimulatedSps(Device):
 
 
 def pitch_pvs(pitchid):
-
-    x = {'P%d:Select' % pitchid : PV('select_%d'%pitchid, read_only=False,
-                                doc='Select pitch', type='int'),
-         'P%d:Selectable' % pitchid : PV('selectable_%d'%pitchid,type='int',
+    x = {
+        'P%d:Select' % pitchid: PV('select_%d' % pitchid, read_only=False,
+                                   doc='Select pitch', type='int'),
+        'P%d:Selectable' % pitchid: PV('selectable_%d' % pitchid, type='int',
                                        read_only=True, doc='Pitch can be '
                                                            'selected'),
-         'P%d:Selected' % pitchid : PV('selected_%d' % pitchid, type='int',
-                                       read_only=True, doc='Pitch is selected'),
-         'P%d:ParkPosition' % pitchid : PV('park_position_%d' % pitchid,
-                                          doc='Pitch is parked', type='int',
-                                          read_only=True),
-         'P%d:Alarm' % pitchid: PV('alarm_%d' % pitchid, read_only=True,
+        'P%d:Selected' % pitchid: PV('selected_%d' % pitchid, type='int',
+                                     read_only=True, doc='Pitch is selected'),
+        'P%d:ParkPosition' % pitchid: PV('park_position_%d' % pitchid,
+                                         doc='Pitch is parked', type='int',
+                                         read_only=True),
+        'P%d:Alarm' % pitchid: PV('alarm_%d' % pitchid, read_only=True,
                                   type='int')
-         }
+        }
     return x
 
 
 class SpsS7EpicsInterface(EpicsInterface):
-    pvs = {'FineAdjustment:Select': PV('fa_select',
-                                        doc='Enable fine adjustment of the '
+    pvs = {
+        'FineAdjustment:Select': PV('fa_select',
+                                    doc='Enable fine adjustment of the '
                                         'mirrors', type='int'),
-           'MCU2:FineAdjustmentSelected': PV('fa_read',
-                                        doc='Fine adjustment selected',
-                                        read_only=True, type='int'),
-           'MCU2:Move': PV('move', doc='NOT WORKING -- Motor is moving',
-                           read_only=True, type='int')
-           }
+        'MCU1:FineAdjustmentSelected': PV('fa_read1',
+                                          doc='Fine adjustment selected',
+                                          read_only=True, type='int'),
+        'MCU2:FineAdjustmentSelected': PV('fa_read2',
+                                          doc='Fine adjustment selected',
+                                          read_only=True, type='int'),
+        'MCU2:Move': PV('move', doc='NOT WORKING -- Motor is moving',
+                        read_only=True, type='int')
+        }
     for pid in range(1, 37):
         pvs.update(pitch_pvs(pid))
 
-
-    _commands = {'start': 'start',
-                 'stop': 'stop',
-                 }
+    _commands = {
+        'start': 'start', 'stop': 'stop',
+        }
 
     _last_command = ''
 
     @property
-
     def execute_command(self):
         """
         Command to execute. Possible commands are start, stop.
@@ -641,20 +699,11 @@ class SpsS7EpicsInterface(EpicsInterface):
         return self._last_command
 
 
-
 setups = dict(
-    sps = dict(
-        device_type=SimulatedSps,
-        parameters=dict(
-            override_initial_data={
-                'selectable_33': 1,
-                'selectable_34': 1,
-                'selectable_35': 1,
-                'selectable_36': 1,
+    sps=dict(device_type=SimulatedSps, parameters=dict(override_initial_data={
+        'selectable_33': 1, 'selectable_34': 1, 'selectable_35': 1,
+        'selectable_36': 1,
 
-            }
-        )
-    )
-)
+        })))
 
 framework_version = '1.2.1'
